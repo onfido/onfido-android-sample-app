634d50cacce19dfce1cc2e091a08b337d176258a
gokhanaliccii <gokhanaliccii@gmail.com>
2019-09-25 14:47:27 +0100

    Update README

--- app/src/main/java/com/onfido/android/app/sample/MainActivity.java
+++ app/src/main/java/com/onfido/android/app/sample/MainActivity.java
@@ -2,7 +2,6 @@ package com.onfido.android.app.sample;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -26,12 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.main);
 
-        findViewById(R.id.next).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startFlow();
-            }
-        });
+        findViewById(R.id.next).setOnClickListener(v -> startFlow());
 
         client = OnfidoFactory.create(this).getClient();
     }
